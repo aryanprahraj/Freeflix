1d2f4dbaada4ae4a44097a5570ff68733733611b
AryanP <aryanp@optimeyes.ai>
2025-11-16 05:15:22 -0500

    Fix: Use environment variables for all Firebase config values

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "freeflix-45feb.firebaseapp.com",
-  projectId: "freeflix-45feb",
-  storageBucket: "freeflix-45feb.appspot.com",
-  messagingSenderId: "154261774339",
-  appId: "1:154261774339:web:4c7946a303e36efdbdee36",
-  measurementId: "G-XK59YZ7CL0"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
