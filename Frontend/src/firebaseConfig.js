a4bfb684e601112f2ffae4b3fcb3b7d3d2870400
AryanP <aryanp@optimeyes.ai>
2025-11-16 04:25:41 -0500

    Update firebaseConfig.js

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,18 +1,9 @@
-// src/firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDI48i3W1op63KMGHK6cjHDeFMH3y9QNaU",
-  authDomain: "freeflix-45feb.firebaseapp.com",
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "freeflix-qzhi.vercel.app",
   projectId: "freeflix-45feb",
   storageBucket: "freeflix-45feb.appspot.com",
   messagingSenderId: "154261774339",
   appId: "1:154261774339:web:4c7946a303e36efdbdee36",
   measurementId: "G-XK59YZ7CL0"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
